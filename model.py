5fcb09c3b975fb9079ba533cce6ee651be47a9f0
Anirudh <63116473+AnirudhJ77@users.noreply.github.com>
2024-11-16 17:09:23 -0600

    added extract_features

--- model.py
+++ model.py
@@ -1,1 +1,14 @@
-print()
+import numpy as np
+import pandas as pd
+
+
+def extract_features(meow):
+    '''
+    Extracts features from a .wav file
+
+    Parameters:
+    meow (file) : vector of features in np array
+
+    Returns
+    numpy.ndarray : vector of features
+    '''
