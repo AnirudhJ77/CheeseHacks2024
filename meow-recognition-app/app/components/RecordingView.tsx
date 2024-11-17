6a0a66907eae0a29bececaa35dceec5cf9cf6d13
Bruno Wu <bruno7405@gmail.com>
2024-11-17 09:10:08 -0600

    Behavior Description UI Update

--- meow-recognition-app/app/components/RecordingView.tsx
+++ meow-recognition-app/app/components/RecordingView.tsx
@@ -11,6 +11,8 @@ export default function RecordingView() {
     const audioChunksRef = useRef<Blob[]>([]);
     const [audioUrl, setAudioUrl] = useState<string | null>(null);
     const [file, setFile] = useState<Blob | null>(null);
+    const [hasResult, setHasResult] = useState<boolean>(false);
+
 
     //#region Audio Recording Logic
     // Convert Float32Array to Int16Array
@@ -43,13 +45,13 @@ export default function RecordingView() {
         const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
 
         // Extract PCM data from the audio buffer
-        const sampleRate = 48000; // Desired sample rate (adjust as needed)
+        const sampleRate = 48000; // (adjust as needed)
         const channelData = audioBuffer.getChannelData(0); // Get data from the first channel
         const pcmData = downsampleBuffer(channelData, audioBuffer.sampleRate, sampleRate);
 
         // Encode PCM data into WAV
         const wav = new WaveFile();
-        wav.fromScratch(1, sampleRate, "16", pcmData); // Mono, desired sample rate, 16-bit
+        wav.fromScratch(1, sampleRate, "16", pcmData); // Mono, sample rate, 16-bit
         return wav.toBuffer();
     };
 
@@ -97,10 +99,14 @@ export default function RecordingView() {
                 const url = URL.createObjectURL(wavBlob);
                 setAudioUrl(url);
                 setRecordingComplete(true);
+
+                setHasResult(true);
             };
 
             mediaRecorder.start();
             setIsRecording(true);
+            setHasResult(false);
+
         } catch (error) {
             console.error("Error accessing microphone:", error);
         }
@@ -126,9 +132,9 @@ export default function RecordingView() {
     return (
         <div className="flex items-center justify-center h-screen w-full">
             <div className="">
-                <h1 className="animate-pulse text-center text-9xl font-mono font-bold text-amber-800 opacity-60">
+                <h1 className="text-center text-9xl font-mono font-bold text-amber-800 opacity-70">
                     ShaMeow
-                    <p className="text-center text-xl font-mono font-bold text-amber-800 opacity-60">
+                    <p className="text-center text-xl font-mono font-bold text-amber-800 opacity-70">
                         Find out what your cat is thinking!
                     </p>
                     <p className="text-center text-s font-mono font-bold text-amber-800 opacity-60">
@@ -147,12 +153,11 @@ export default function RecordingView() {
                                     d="M12 6h-2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2zm10 0h-2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2z" />
                             </svg>
                             <span className="animate-ping absolute inline-flex h-40 w-40 rounded-full bg-red-300 opacity-75 z-0"></span>
-
                         </button>
 
                     ) : (
                         <button onClick={handleToggleRecording}
-                            className="rounded-full w-40 h-40 mt-10 m-auto flex items-center justify-center bg-amber-500 opacity-80 hover:bg-amber-600"
+                            className="animate-pulse rounded-full w-40 h-40 mt-10 m-auto flex items-center justify-center bg-amber-500 opacity-80 hover:bg-amber-600"
                         >
                             <svg
                                 className="w-24 h-24"
@@ -166,11 +171,12 @@ export default function RecordingView() {
                         </button>
                     )}
                 </div>
+                {/*
                 <div className="pt-5 pb-5">
                     {recordingComplete && audioUrl && (
-                        <div className="flex items-center w-full">
+                        <div className="flex items-center justify-center w-full">
                             <audio controls src={audioUrl}></audio>
-                            <a className="pl-5"
+                            <a className="flex items-center justify-center pl-5"
                                 href={audioUrl}
                                 download="cat-audio.wav">
                                 Download WAV
@@ -178,11 +184,15 @@ export default function RecordingView() {
                         </div>
                     )}
                 </div>
+                */}
+
                 <div className="flex items-center justify-center">
-                    <p className="bg-amber-600 bg-opacity-20 w-full md:w-[800px] h-32 p-4 text-balance text-center text-s font-mono font-bold text-amber-800 opacity-60">
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    </p>
+                    <div className={`transition-opacity duration-500 ${hasResult ? "opacity-100" : "opacity-0"} 
+                    bg-opacity-20 w-full md:w-[800px] h-20 p-4 text-balance text-center text-3xl font-mono font-bold text-amber-800`}>
+                        <p className=" bg-opacity-20 w-full md:w-[800px] h-20 p-4 text-balance text-center text-3xl font-mono font-bold text-amber-800 opacity-50">
+                            Your cat is hungry
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
